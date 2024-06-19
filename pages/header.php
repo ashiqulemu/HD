555099ab28589b83d5682a9ac1ed33040c8347f7
ashiqulemu <ashiqulemu.jpi@gmail.com>
2024-06-19 19:15:53 +0600

    first commit

---
+++ pages/header.php
@@ -0,0 +1,98 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- seo activities -->
+    <meta charset="utf-8">
+    <meta name="google-site-verification" content="b3JBEB_Tp6NzGBZBo-wCS3L3RNdS1WNSiqZdCdt0gDw" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta property='og:title' content=" " />
+    <meta property='og:image' content='./images/ ' />
+    <meta property='og:description' content=' ' />
+    <meta property='og:url' content='ashiqulemu.netlify.app' />
+    <meta property='og:image:width' content='1200' />
+    <meta property='og:image:height' content='627' />
+    <meta property="og:type" content='website' />
+    <title>HD Accountancy</title>
+    <link rel="icon" href="./images/favi.png">
+
+    <!-- css -->
+    <link href="../css/bootstrap-5.2.3.min.css" rel="stylesheet">
+    <link href="../css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/animate.min.css" />
+
+    <!-- slick css -->
+    <link rel="stylesheet" type="text/css" href="./css/slick.css" />
+    <link rel="stylesheet" type="text/css" href="./css/slick-theme.css" /> 
+
+    <!--js animation -->
+    <script type="text/javascript" src="./js/wow.min.js"></script>
+
+    <!-- iconify icon -->
+    <script src="./js/iconify.min.js"></script>
+</head>
+
+<body>
+
+    <!-- site header start -->
+    <header class="site-header"> 
+        <section class="container">
+            <nav class="navbar navbar-expand-lg py-3">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">
+                        <img src="../images/logo.png" width="160px" class="img-fluid" alt="df">
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 navCustom">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                    aria-expanded="false">
+                                    our service
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item text-capitalize fw-bold " href="#">accounting solution </a></li>
+                                    <li><a class="dropdown-item text-capitalize fw-bold " href="#">tax solution </a></li>
+                                    <li><a class="dropdown-item text-capitalize fw-bold " href="#">other solution </a></li>
+                                    
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link" >pricing</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link" >about us</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link" >contact</a>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item bg-primary box-hover rounded-3  ">
+                                <a href="#" class="nav-link text-white text-center fw-bold fs-6 px-4" aria-current="page" href="#">Book Appointment <br>
+                               <span class="fs-5 d-flex gap-1 align-items-center">
+                               <iconify-icon icon="charm:phone"></iconify-icon>  01484 508951</span>
+                            </a>
+                            </li>
+
+                        </ul>
+                        <!-- <form class="d-flex" role="search">
+                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                            </form> -->
+                    </div>
+                </div>
+            </nav>
+        </section>
+    </header>
+
+    <!-- site header end -->
