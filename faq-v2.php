e51475df64ed9aa0e0fa435f3864310c879ec1e8
ashiqulemu <ashiqulemu.jpi@gmail.com>
2024-09-23 00:12:10 +0600

    werewr

---
+++ faq-v2.php
@@ -0,0 +1,298 @@
+<!-- global Header -->
+<?php include './header.php'; ?>
+
+<section class="py-5 bg-primary">
+    <div class="container-fluid ">
+        <div class="row">
+            <div class="com-md-12 pt-5">
+                <div class="text-center">
+                    <h1 class="text-light poppins-bold">We can help setting up new susiness</h1>
+                    <p class="text-light mx-auto py-3 w-75  ">
+                        Taking the leap into enterepreneurship is exciting. but navigation the initial setup process can fell daunting. at HD account services, we're passionate about empowering new budiness owners like you, we offer comprehensive support for both sole traders and limited companies, guiding you through every step and ensuring a smooth and successful launch.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<section class="py-5">
+    <div class="container">
+        <div class="row pb-5">
+            <div class="col-md-12">
+                <div>
+                    <div class="text-center">
+                        <h1 class="txt-primary text-center text-capitalize poppins-bold">Choosing the Right Business Structure</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row ">
+            <div class="col-lg-7">
+                <div>
+                    <div>
+                        <p class="txt-primary text-capitalize fw-medium">The first crucial decision is selecting the optimal business structure for your goals. We understand that understanding the strategic financial benefits of each option can be complex. Don't worry, we're here to help! Through personalized consultations, we will:</p>
+                    </div>
+                    <div class="py-4">
+                        <ul class="ps-3">
+                            <li class="txt-primary fw-bold">
+                                <h6 class="poppins-bold">Analyze Your Business Goals </h6>
+                                <p class="fw-medium text-capitalize mb-1">We'll discuss your vision, projected growth, and risk tolerance to understand your unique needs.</p>
+                            </li>
+                            <li class="txt-primary fw-bold ">
+                                <h6 class="poppins-bold">Explain Sole Trader &amp; Limited Company Structures</h6>
+                                <p class="fw-medium text-capitalize mb-1">We'll provide clear explanations of each structure, including tax implications, liability protection, and administrative requirements.</p>
+                            </li>
+                            <li class="txt-primary fw-bold ">
+                                <h6 class="poppins-bold">Recommend the Best Fit </h6>
+                                <p class="fw-medium text-capitalize mb-1">Based on our understanding of your goals and financial situation, we'll recommend the structure that best positions you for success.</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <div>
+                    <div class="card text-center card-hover p-3 rounded-0 bg-light-blue mb-3" style="background:#C9D3E4;">
+                        <h6 class="txt-primary fw-bold poppins-bold" style="font-size: 26px;">Beyond Setup we are Your Ongoing Partner</h6>
+                    </div>
+                    <div class="card text-center card-hover rounded-4 py-2 my-3">
+                        <h6 class="txt-primary  poppins-bold" style="font-size: 26px;">Bookkeeping &amp; <br> Accounting Solutions</h6>
+                    </div>
+                    <div class="card text-center card-hover rounded-4 py-2 my-3">
+                        <h6 class="txt-primary  poppins-bold" style="font-size: 26px;">Tax Planning &amp; <br> Optimization</h6>
+                    </div>
+                    <div class="card text-center card-hover rounded-4 py-3 my-3">
+                        <h6 class="txt-primary  poppins-bold" style="font-size: 26px;">Strategic Financial Advice</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="pt-5">
+    <div class="container ">
+        <div class="row ">
+            <div class="col-lg-12">
+                <div class="text-center">
+                    <h1 class="txt-primary poppins-bold">Expert Support for Every Step of Your Startup</h1>
+                </div>
+            </div>
+        </div>
+
+        <!-- Step -->
+        <div class="row py-5 mt-5">
+            <!-- step-1 -->
+            <div class="col-lg-6 m-0 p-0"></div>
+            <div class="col-lg-6 border-left-custom pb-5 m-0 p-0">
+                <div class="px-5 ms-3 mb-md-5 mb-3">
+                    <h6 class="txt-primary poppins-bold text-capitalize">carefully listen to your plan</h6>
+                    <p class="txt-primary fw-medium">We carefully listen to understand your business <br> idea, goals, and vision
+                        <br>Discuss your background, skills, and experience to understand the reality of your business.
+                    </p>
+                </div>
+                <div class="rounded-circle step-box step-left text-light text-center">
+                    <div style="    line-height: 0.8;">
+                        <h1 class="m-0 poppins-bold">1</h1> <small>step</small>
+                    </div>
+                </div>
+            </div>
+            <!-- step-2 -->
+            <div class="col-lg-6 border-right-custom pb-5  m-0 p-0">
+                <div class="px-5 text-end me-3 mb-md-5 mb-3">
+                    <h6 class="txt-primary poppins-bold text-capitalize">Sole trade VS Limited company argument</h6>
+                    <p class="txt-primary fw-medium"> Explain different business structures Ex: sole trader, partnership, limited company. Advise you on the most suitable structure based on your situation and future goals. </p>
+                </div>
+                <div class="rounded-circle step-box step-right text-light text-center">
+                    <div style="line-height:0.8;">
+                        <h1 class="m-0 poppins-bold">2</h1> <small>step</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 m-0 p-0"></div>
+            <!-- step-3 -->
+            <div class="col-lg-6 m-0 p-0"></div>
+            <div class="col-lg-6 border-left-custom pb-5 m-0 p-0">
+                <div class="px-5 ms-3 mb-md-5 mb-3">
+                    <h6 class="txt-primary poppins-bold text-capitalize">Business Registration</h6>
+                    <p class="txt-primary fw-medium">Guide you through the necessary legal requirements, including business name registration, trademarks, licenses, and permits. Assist with the registration process, such as setting up a limited company with Companies House.
+                    </p>
+                </div>
+                <div class="rounded-circle step-box step-left text-light text-center">
+                    <div style="    line-height: 0.8;">
+                        <h1 class="m-0 poppins-bold">3</h1> <small>step</small>
+                    </div>
+                </div>
+            </div>
+            <!-- step-4 -->
+            <div class="col-lg-6 border-right-custom pb-5  m-0 p-0">
+                <div class="px-5 text-end me-3 mb-md-5 mb-3">
+                    <h6 class="txt-primary poppins-bold text-capitalize">Opening a Business Bank Account</h6>
+                    <p class="txt-primary fw-medium">Advise on the importance of separating personal and business finances. Help you to choose a suitable business bank account and assist with the application process. </p>
+                </div>
+                <div class="rounded-circle step-box step-right text-light text-center">
+                    <div style="line-height:0.8;">
+                        <h1 class="m-0 poppins-bold">4</h1> <small>step</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 m-0 p-0"></div>
+            <!-- step-5 -->
+            <div class="col-lg-6 m-0 p-0"></div>
+            <div class="col-lg-6 border-left-custom pb-5 m-0 p-0">
+                <div class="px-5 ms-3 mb-md-5 mb-3">
+                    <h6 class="txt-primary poppins-bold text-capitalize">Setting Up Digital Bookkeeping and <br> Accounting Systems</h6>
+                    <p class="txt-primary fw-medium"> Recommend and set up accounting software (ex- QuickBooks, Xero). Train you on using the software, Set up processes for invoicing, expense tracking, and some useful quick reporting.
+                    </p>
+                </div>
+                <div class="rounded-circle step-box step-left text-light text-center">
+                    <div style="    line-height: 0.8;">
+                        <h1 class="m-0 poppins-bold">5</h1> <small>step</small>
+                    </div>
+                </div>
+            </div>
+            <!-- step-6 -->
+            <div class="col-lg-6 border-right-custom pb-5  m-0 p-0">
+                <div class="px-5 text-end me-3 mb-md-5 mb-3">
+                    <h6 class="txt-primary poppins-bold text-capitalize">Tax Planning and Advice</h6>
+                    <p class="txt-primary fw-medium">Explain the different types of taxes the business will be subject to (corporation tax, VAT, PAYE, CIS). Provide strategies for tax efficiency.
+                        Schedule regular tax reviews to ensure compliance and optimize tax planning</p>
+                </div>
+                <div class="rounded-circle step-box step-right text-light text-center">
+                    <div style="line-height:0.8;">
+                        <h1 class="m-0 poppins-bold">6</h1> <small>step</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 m-0 p-0"></div>
+            <!-- step-7 -->
+            <div class="col-lg-6 m-0 p-0"></div>
+            <div class="col-lg-6 border-left-custom pb-5 m-0 p-0">
+                <div class="px-5 ms-3 mb-md-5 mb-3">
+                    <h6 class="txt-primary poppins-bold text-capitalize">Funding and Finance</h6>
+                    <p class="txt-primary fw-medium"> Discuss different funding options (loans, investors, grants). Assist with preparing loan applications if require. Advise on cash flow management and financial planning.
+                    </p>
+                </div>
+                <div class="rounded-circle step-box step-left text-light text-center">
+                    <div style="    line-height: 0.8;">
+                        <h1 class="m-0 poppins-bold">7</h1> <small>step</small>
+                    </div>
+                </div>
+            </div>
+            <!-- step-8 -->
+            <div class="col-lg-6 border-right-custom pb-5  m-0 p-0">
+                <div class="px-5 text-end me-3 mb-md-5 mb-3">
+                    <h6 class="txt-primary poppins-bold text-capitalize">Ongoing Support</h6>
+                    <p class="txt-primary fw-medium">Schedule regular check-ins to review business performance and provide advice. Offer unlimited support via phone, email, text, and face-to-face meetings. Keep you updated on changes in tax laws and regulations.</p>
+                </div>
+                <div class="rounded-circle step-box step-right text-light text-center">
+                    <div style="line-height:0.8;">
+                        <h1 class="m-0 poppins-bold">8</h1> <small>step</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 m-0 p-0"></div>
+            <!-- step-9 -->
+            <div class="col-lg-6 m-0 p-0"></div>
+            <div class="col-lg-6 border-left-custom pb-5 m-0 p-0">
+                <div class="px-5 ms-3 mb-md-5 mb-3">
+                    <h6 class="txt-primary poppins-bold text-capitalize">Half-Yearly Tax Position Review</h6>
+                    <p class="txt-primary fw-medium"> Conduct an annual review of you business’s financial health. Adjust the business plan, accounting practices, and tax strategies as needed. Plan for the next financial year and set new goals.
+                    </p>
+                </div>
+                <div class="rounded-circle step-box step-left text-light text-center">
+                    <div style="line-height: 0.8;">
+                        <h1 class="m-0 poppins-bold">9</h1> <small>step</small>
+                    </div>
+                </div>
+            </div>
+            <!-- step-10 -->
+            <div class="col-lg-6  border-right-custom position-relative pb-5  m-0 p-0">
+                <div class="px-5 text-end me-3 mb-md-5 mb-3">
+                    <h6 class="txt-primary poppins-bold text-capitalize">Ongoing Support</h6>
+                    <p class="txt-primary fw-medium">Review your business’s tax position mid-year to ensure no surprises at year-end. Adjust tax planning strategies as needed based on business performance</p>
+                </div>
+                <div class="rounded-circle step-box step-right text-light text-center">
+                    <div style="line-height:0.8;">
+                        <h1 class="m-0 poppins-bold">10</h1> <small>step</small>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 m-0 p-0"></div>
+
+        </div>
+
+        <!-- Thinking about setting up new Business? -->
+        <div class="row pb-5 mb-md-5 mb-0">
+            <div class="col-lg-12">
+                <div class="text-center">
+                    <h1 class="txt-primary poppins-bold text-capitalize">Thinking about setting up new Business?</h1>
+                </div>
+                <div class="text-center py-4">
+                <a href="./get-quotation.php" class="btn bg-primary py-2 px-5 poppins-bold text-white"> Book your Appointment</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<section class="pb-5">
+        <div class="container ">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center">
+                        <h2 class="txt-primary poppins-bold text-capitalize">Frequently Asked Questions</h2>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row py-4">
+                <div class="text-center">
+                    <div class="accordion accordion-flush w-75 mx-auto" id="accordionFlushExample">
+
+                        <div class="accordion-item my-3 shadow border border-1 rounded-4 overflow-hidden">
+                          <h2 class="accordion-header  ">
+                            <button class="accordion-button collapsed txt-primary poppins-bold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                How long does it take to set up a new business?
+                            </button>
+                          </h2>
+                          <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit necessitatibus error ullam cupiditate, ad, recusandae eligendi nesciunt hic quo blanditiis nemo totam dolorum esse dolor distinctio non, eos vero architecto asperiores eveniet doloremque amet. Neque eos aspernatur aliquid et consectetur animi fugit aperiam fuga accusantium dolorem facilis quo nisi, </div>
+                          </div>
+                        </div>
+
+                        <div class="accordion-item my-3 shadow border border-1 rounded-4 overflow-hidden">
+                          <h2 class="accordion-header ">
+                            <button class="accordion-button collapsed txt-primary poppins-bold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                Do I need to register with HMRC if I'm a Sole Trader?
+                            </button>
+                          </h2>
+                          <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit necessitatibus error ullam cupiditate, ad, recusandae eligendi nesciunt hic quo blanditiis nemo totam dolorum esse dolor distinctio non, eos vero architecto asperiores eveniet doloremque amet. Neque eos aspernatur aliquid et consectetur animi fugit aperiam fuga accusantium dolorem facilis quo nisi, </div>
+                          </div>
+                        </div>
+
+                        <div class="accordion-item my-3 shadow border border-1 rounded-4 overflow-hidden">
+                          <h2 class="accordion-header ">
+                            <button class="accordion-button collapsed txt-primary poppins-bold fs-5" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                                What are the main differences between a Sole Trader and a Limited Company?
+                            </button>
+                          </h2>
+                          <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                            <div class="accordion-body text-start">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit necessitatibus error ullam cupiditate, ad, recusandae eligendi nesciunt hic quo blanditiis nemo totam dolorum esse dolor distinctio non, eos vero architecto asperiores eveniet doloremque amet. Neque eos aspernatur aliquid et consectetur animi fugit aperiam fuga accusantium dolorem facilis quo nisi, </div>
+                          </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+<!-- global footer -->
+<?php include './footer.php'; ?>
